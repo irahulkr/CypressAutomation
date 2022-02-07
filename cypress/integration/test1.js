1b6a7b3e14a33949085167ebb6b9d91548441828
Rahul Kumar <Rahulkumar1999.rk@gmail.com>
2022-02-07 13:25:42 +0530

    FIrst test

---
+++ cypress/integration/test1.js
@@ -0,0 +1,13 @@
+describe('My first Test Suite', function() 
+{
+    it('My First Test Case', function()
+    {
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+
+    })
+
+    it('My Second Test Suite', function()
+    {
+
+    })
+})
