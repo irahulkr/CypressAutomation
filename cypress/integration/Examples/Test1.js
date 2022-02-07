295933603dadff041fe9832d4ba33d083c435cf8
Rahul Kumar <Rahulkumar1999.rk@gmail.com>
2022-02-07 16:44:30 +0530

    Add to cart

--- cypress/integration/Examples/Test1.js
+++ cypress/integration/Examples/Test1.js
@@ -1,8 +1,28 @@
+/// <reference types="Cypress" />
+
 describe('My first Test Suite', function() 
 {
     it('My First Test Case', function()
     {
-        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        cy.get('.search-keyword').type('Ca');
+        cy.wait(2000);
+        cy.get('.product:visible').should('have.length',4);
+
+        //Parent Child Chaining
+        cy.get('.products').find('.product').should('have.length',4);
+
+        //Add to card for 2nd product
+        cy.get('.products').find('.product').eq(1).contains("ADD TO CART").click();
+
+        //3rd method using loop
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+            const textVeg = $el.find('h4.product-name').text();
+            if(textVeg.includes('Cashews'))
+            {
+                cy.wrap($el).find('button').click();
+            }
+        })
 
     })
 
@@ -11,3 +31,11 @@ describe('My first Test Suite', function()
 
     })
 })
+
+// Fixture- Store data (Auto5.json)
+//Integration - Write all test cases
+//Plugins - Listeners - full screen browsers, what to do before event is happening
+//Support - Reusable methods / Action file
+//Videos - For test run we will have videos stored here
+//Node modules - Created when we hit npn install cypress. Heart of this project
+//Cypress.json - Congifuration: timeout
