3540623c22ea4dbea8e5a958a489087e30a60138
Rahul Kumar <Rahulkumar1999.rk@gmail.com>
2022-02-15 17:21:57 +0530

    Page object model

--- cypress/integration/Examples/Test8Framework.js
+++ cypress/integration/Examples/Test8Framework.js
@@ -50,6 +50,14 @@ describe('Cypress Framework', function()
         cy.contains('Checkout').click()
         cy.get('#country').type('India')
         cy.get('.suggestions > ul > li > a').click()
+        cy.get('#checkbox2').click({force:true})
+        cy.get('input[type="submit"]').click()
+        //cy.get('.alert').should('have.text','Success! Thank you! Your order will be delivered in next few weeks :-).')
+        cy.get('.alert').then(function(element)
+        {
+            const actualText=element.text()
+            expect(actualText.includes("Success")).to.be.true
+        })
 
     })
 })
